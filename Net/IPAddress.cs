35503646dc3dbd2e9394e1f02e054b8e6327c834
Simon Walker <github@helpmebot.org.uk>
2010-01-31 20:39:02 +0000

    add new functions to make IPAddress objects

---
+++ Net/IPAddress.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Utility.Net
+{
+   public class IPAddress 
+    {
+        public static System.Net.IPAddress newFromString( string ipText )
+        {
+            string[ ] ipOctets = ipText.Split( '.' );
+            byte[ ] ipAddr = new byte[ 4 ];
+            for( int i = 0; i < 4; i++ )
+            {
+                ipAddr[ i ] = byte.Parse( ipOctets[ i ], System.Globalization.NumberStyles.Integer );
+            }
+
+           return new System.Net.IPAddress( ipAddr );
+        }
+
+        public static System.Net.IPAddress newFromEncodedString( byte[ ] IPv4Address )
+        {
+            return newFromString( ASCIIEncoding.ASCII.GetString( IPv4Address ) );
+        }
+    }
+}

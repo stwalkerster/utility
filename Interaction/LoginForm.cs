bf98d075ef97b5ed177a1bfae2614e97af848d11
Simon Walker <github@helpmebot.org.uk>
2011-05-11 16:44:31 +0000

    login form

---
+++ Interaction/LoginForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Utility.Interaction
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        public string getUsername()
+        {
+            return textBox1.Text;
+        }
+
+        public string getPassword()
+        {
+            return textBox2.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}

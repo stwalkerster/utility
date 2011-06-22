5cc9b6ef6b19f5565002479351e0f9bdf8dff6f8
Simon Walker <github@helpmebot.org.uk>
2011-06-22 22:29:20 +0100

    publish stuff etc

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "ConsoleMenu" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyTitle("Utility Library")]
+[assembly: AssemblyDescription("Stwalkerster's main library of functions")]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "ConsoleMenu" )]
+[assembly: AssemblyCompany("Simon Walker")]
+[assembly: AssemblyProduct("Utility")]
 [assembly: AssemblyCopyright( "Copyright ©  2009" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
---
+++ README.md
@@ -0,0 +1,5 @@
+# Utility library
+
+This is my main utility library which is linked to from a lot of my projects.
+
+See LICENCE.md for licencing stuff.

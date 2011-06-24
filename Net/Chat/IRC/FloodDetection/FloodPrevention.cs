5af5df180db6597c777ce641ff25ce69dde5e8ac
Simon Walker <github@helpmebot.org.uk>
2011-06-24 19:17:39 +0100

    basic flood detection abstract class. definitely needs expanding properly.

---
+++ Net/Chat/IRC/FloodDetection/FloodPrevention.cs
@@ -0,0 +1,10 @@
+namespace Utility.Net.Chat.InternetRelayChat.FloodDetection
+{
+    public abstract class FloodPrevention
+    {
+        /// <summary>
+        /// synchronous method to force the IRC client to wait if required
+        /// </summary>
+        public abstract void wait();
+    }
+}

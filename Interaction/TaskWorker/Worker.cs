eea679bf902ef261b2d491f7866e7b7f5b14a6cf
Simon Walker <github@helpmebot.org.uk>
2011-06-24 18:23:19 +0100

    worker start

---
+++ Interaction/TaskWorker/Worker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Utility.Interaction.TaskWorker
+{
+    public partial class Worker : Form
+    {
+        public Worker()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The text that appears on the main form itself.
+        /// </summary>
+        public string Description
+        {
+            get { return label1.Text; }
+            set { label1.Text = value;}
+        }
+
+        public void DoWork(params WorkerTask[] tasks)
+        {
+            
+        }
+
+
+        private void Worker_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Interaction/TaskWorker/WorkerTaskParameters.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Interaction.TaskWorker
+{
+    public class WorkerTaskParameters
+    {
+
+    }
+}

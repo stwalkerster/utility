9a651fb648b0a2192e43a5d27f20f224b964099a
Simon Walker <github@helpmebot.org.uk>
2011-07-07 21:11:30 +0100

    exception for already-connected stuff

---
+++ Net/Chat/InternetRelayChat/Exceptions/AlreadyConnectedException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Net.Chat.InternetRelayChat.Exceptions
+{
+    class AlreadyConnectedException : System.Exception
+    {
+        public override string Message { get { return "The client is already connected to the network"; } }
+    }
+}

424ec3ba54a4b69c9f648056f75494df180fc382
Simon Walker <github@helpmebot.org.uk>
2011-07-07 21:04:18 +0100

    add event handlers

---
+++ Net/Chat/InternetRelayChat/EventHandlers/DataAvailableEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Net.Chat.InternetRelayChat.EventHandlers
+{
+    public class DataAvailableEventArgs : IrcEventArgs
+    {
+        /// <summary>
+        /// The data that is available
+        /// </summary>
+        public string data { get; set; }
+
+        /// <summary>
+        /// Creates a new instance with the specified data that is available.
+        /// </summary>
+        /// <param name="data"></param>
+        public DataAvailableEventArgs(string data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ Net/Chat/InternetRelayChat/EventHandlers/IrcEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Net.Chat.InternetRelayChat.EventHandlers
+{
+    public class IrcEventArgs : EventArgs
+    {
+    }
+}

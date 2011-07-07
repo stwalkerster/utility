36e37c7fcbeeea29707f0cca30876ff4e80374ad
Simon Walker <github@helpmebot.org.uk>
2011-07-07 21:34:31 +0100

    simple timer flood protect - currently the default behaviour for helpmebot

---
+++ Net/Chat/InternetRelayChat/FloodDetection/SimpleTimerFloodPrevention.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Utility.Net.Chat.InternetRelayChat.FloodDetection
+{
+    class SimpleTimerFloodPrevention : FloodPrevention
+    {
+        #region Overrides of FloodPrevention
+
+        /// <summary>
+        /// synchronous method to force the IRC client to wait if required
+        /// </summary>
+        public override void wait(string message)
+        {
+            Thread.Sleep(500);
+        }
+
+        #endregion
+    }
+}
